af5145a6e2ba33eae34c9adfae67f6d429c40517
Arthur Silva Lima <arthusil18@hotmail.com>
2023-03-14 02:13:52 -0300

    criando metodo post e delete no front

--- src/components/CadastroPessoa.js
+++ src/components/CadastroPessoa.js
@@ -1,36 +1,92 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
+import api from "../services/api";
 
 function CadastroPessoa() {
 
     const formInicio = {
-        nome: "",
-        cpf: "",
-        rg: "",
-        telefone: "",
+        nome: "", 
+        rg: "", 
+        telefone: "", 
         rua: "",
         bairro: "",
-        numero: "",
         cep: "",
-        estado: "",
         cidade: "",
-        pais: "",
+        cpf: "",
+        estado: "",
+        numero: "",
+        pais: ""
     }
 
     const [pessoa, setPessoa] = useState(formInicio);
 
     const setInput = (value) => {
-        setForm(pessoa => ({...pessoa, ...value}))
+        setPessoa(pessoa => ({...pessoa, ...value}))
     }
 
+    const cadastrarPessoa = (e) => {
+        console.log(pessoa)
 
+        api.post("/pessoa/add", pessoa)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+        e.preventDefault();
+        setPessoa(formInicio)
+        
+    }
     return (
         <div className="form-cadastro">
             <h1>Cadastrar pessoa</h1>
-            <form onSubmit={cadastrarPessoa()}>
+            <form onSubmit={(e) => cadastrarPessoa(e)}>
                 <label>
                     Nome:
                     <input type="text" value={pessoa.nome} onChange={e => setInput({nome: e.target.value})}/>
                 </label>
+                <label>
+                    CPF:
+                    <input type="text" value={pessoa.cpf} onChange={e => setInput({cpf: e.target.value})}/>
+                </label>
+                <label>
+                    RG:
+                    <input type="text" value={pessoa.rg} onChange={e => setInput({rg: e.target.value})}/>
+                </label>
+                <label>
+                    Telefone:
+                    <input type="text" value={pessoa.telefone} onChange={e => setInput({telefone: e.target.value})}/>
+                </label>
+                <label>
+                    Rua:
+                    <input type="text" value={pessoa.rua} onChange={e => setInput({rua: e.target.value})}/>
+                </label>
+                <label>
+                    Bairro:
+                    <input type="text" value={pessoa.bairro} onChange={e => setInput({bairro: e.target.value})}/>
+                </label>
+                <label>
+                    Número:
+                    <input type="text" value={pessoa.numero} onChange={e => setInput({numero: e.target.value})}/>
+                </label>
+                <label>
+                    CEP:
+                    <input type="text" value={pessoa.cep} onChange={e => setInput({cep: e.target.value})}/>
+                </label>
+                <label>
+                    Estado:
+                    <input type="text" value={pessoa.estado} onChange={e => setInput({estado: e.target.value})}/>
+                </label>
+                <label>
+                    Cidade:
+                    <input type="text" value={pessoa.cidade} onChange={e => setInput({cidade: e.target.value})}/>
+                </label>
+                <label>
+                    Pais:
+                    <input type="text" value={pessoa.pais} onChange={e => setInput({pais: e.target.value})}/>
+                </label>
+
+                <input type="submit" value="Submit"/>
             </form>
         </div>
     )
--- src/pages/Pessoas/index.js
+++ src/pages/Pessoas/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import CadastroPessoa from "../../components/CadastroPessoa";
 import api from "../../services/api";
 
 function Pessoas() {
@@ -10,10 +11,26 @@ function Pessoas() {
             setPessoas(response.data.dados)
         })
         .catch((error) => { 
-            console.error("Erro ao listar funcionários!!!")
+            console.error("Erro ao listar pessoas!!!")
         })
     }, [])
 
+    function editarPessoa(e) {
+        console.log("editar")
+        e.preventDefault();
+    }
+
+    function removerPessoa(e, id) {
+        api.delete(`/pessoa/remove/${id}`)
+        .then((response) => {
+            console.log(response)
+        })
+        .catch((error) => {
+            console.error(error)
+        })
+        e.preventDefault();
+        
+    }
     return (
         <div className="container">
             <h1>Listagem de pessoas cadastradas no sistema</h1>
@@ -24,13 +41,17 @@ function Pessoas() {
                         pessoas.map((pessoa, index) => (
                             <li className="list-item list-item-green" key={index}>
                                 {pessoa.nome}
+                                <button onClick={(e) => editarPessoa(e)}>Editar</button>
+                                <button onClick={(e) => removerPessoa(e, pessoa.codigo)}>Remover</button>
                             </li>
                         ))
                     }
                     </ul>
                 </div>
             </div>
-
+            <div className="cadastro-container">
+                <CadastroPessoa/>
+            </div>
         </div>
     )
 }
